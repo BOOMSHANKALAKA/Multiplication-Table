42b92c931aa64f3c59c0f4a80319adff9c0d74f8
Talon Huang <31824095+BOOMSHANKALAKA@users.noreply.github.com>
2017-12-29 22:18:54 -0500

    Add files via upload

---
+++ Multiplication Table.py
@@ -0,0 +1,7 @@
+# ************ My first success python program ************
+# ************ It is just displaying a multiplication table ************
+# ************ It was programmed on Dec 29th, 2017 ************
+
+for second in range (1, 10):
+   for number in range(1, 10):
+    print(second, "*", number, "=", second*number);
